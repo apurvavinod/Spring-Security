c6547f3abfadba9fdc8adf7a0e1364ea74233dfa
unknown <vinodapurva22@gmail.com>
2024-06-21 20:48:48 -0400

    Initial commit for spring-boot-security project

---
+++ spring-boot-security/src/main/java/org/javabrains/springbootsecurity/security/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package org.javabrains.springbootsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub		
+		auth.inMemoryAuthentication()
+		.withUser("apurva")
+		.password("test")
+		.roles("USER")
+		.and()
+		.withUser("vishakha")
+		.password("test")
+		.roles("ADMIN");
+	}
+	
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		//.antMatchers("/**").hasRole("ADMIN")		
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasAnyRole("USER", "ADMIN")
+		.antMatchers("/").permitAll()
+		.and().formLogin();
+	}
+	
+}
